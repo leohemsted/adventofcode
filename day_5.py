6dca7a720d1a2b2b7693e6c376311dc925667b50
Leo Hemsted <leohemsted@gmail.com>
2016-12-06 23:38:59 +0000

    day 5 part two
    really struggled just aligning types here - got myself confused a lot between strs and ints and what i can safely cast to int or get from the dict, but got there in the end - the slow runtime really hurt me. maybe if i had some tests *shrug*

--- day_5.py
+++ day_5.py
@@ -2,13 +2,23 @@ import hashlib
 import itertools
 
 door_id = 'uqwqemis'
-password = ''
+password = {str(i): None for i in range(8)}
 for index in itertools.count():
     token = door_id + str(index)
     hashed = hashlib.md5(token.encode('utf-8')).hexdigest()
     if hashed[:5] == '00000':
-        print('found match')
-        password += hashed[5]
-        if len(password) == 8:
-            print(password)
+        pword_index = hashed[5]
+        pword_val = hashed[6]
+        if pword_index not in password:
+            print(pword_index, 'out of range')
+            continue
+        if password[pword_index]:
+            print('discarding', pword_index, 'already taken')
+            continue
+
+        print('setting index', pword_index, 'to', pword_val)
+        password[pword_index] = pword_val
+
+        if None not in password.values():
             break
+print(''.join(password[str(i)] for i in range(8)))
