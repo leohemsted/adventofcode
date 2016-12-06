433c7bbc7545c78fbe5f3f3f9e0ef6d02f9df5ce
Leo Hemsted <leohemsted@gmail.com>
2016-12-06 16:31:34 +0000

    day 4 part two
    caesar cypher (with skip for '-' only)
    
    print whole dict, scroll through dict manually as not sure of exact
    wording of key

--- day_4.py
+++ day_4.py
@@ -2,12 +2,46 @@ import collections
 import itertools
 import re
 
+PART_ONE = False
+
 TEST_DATA = [
     'aaaaa-bbb-z-y-x-123[abxyz]',
     'a-b-c-d-e-f-g-h-987[abcde]',
     'not-a-real-room-404[oarel]',
     'totally-real-room-200[decoy]'
 ]
+
+def part_one(valid_rooms):
+    return sum(room['sector_id'] for room in valid_rooms)
+
+def part_two(valid_rooms):
+    decrypted = dict(
+        (
+            ''.join(caeser_cipher(room['name'], room['sector_id'])),
+            room['sector_id']
+        )
+        for room in valid_rooms
+    )
+    from pprint import pprint
+    pprint(decrypted)
+
+
+def caeser_cipher(text, key):
+    def shift_char(c, key):
+        key %= 26
+        if c == '-':
+            return '-'
+        else:
+            char_code = ord(c) + key
+            if char_code > ord('z'):
+                char_code -= 26
+            return chr(char_code)
+    return ''.join(shift_char(c, key) for c in text)
+
+
+part_fn = part_one if PART_ONE else part_two
+
+
 def get_rooms():
     for room in open('day_4_input.txt'):
         match = re.match(
@@ -20,8 +54,8 @@ def get_rooms():
         )
         yield {
             'orig': room,
-            'name': match.group('name').replace('-', ''),
-            'sector_id': match.group('sector_id'),
+            'name': match.group('name'),
+            'sector_id': int(match.group('sector_id')),
             'checksum': match.group('checksum')
         }
 
@@ -32,14 +66,14 @@ def most_common_alphabetical(counter):
         itertools.groupby(counter.most_common(), lambda x: x[1])
     )
 
-def most_common_five(counter):
-    return itertools.islice(most_common_alphabetical(counter), 5)
+def most_common_five(sorted_counter):
+    return itertools.islice(most_common_alphabetical(sorted_counter), 5)
 
-def valid_sector_ids(rooms):
-    for room in rooms:
-        counter = collections.Counter(room['name'])
+def valid_rooms():
+    for room in get_rooms():
+        counter = collections.Counter(room['name'].replace('-', ''))
         computed_checksum = ''.join(x[0] for x in most_common_five(counter))
         if room['checksum'] == computed_checksum:
-            yield int(room['sector_id'])
+            yield room
 
-print(sum(valid_sector_ids(get_rooms())))
+print(part_fn(valid_rooms()))
