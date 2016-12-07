0404f5cd5b0e3c3b6e00bc2521f78b2f57960bd6
Leo Hemsted <leohemsted@gmail.com>
2016-12-07 00:08:09 +0000

    day 6 part two
    another easy section - just grab the end of Counter

--- day_6.py
+++ day_6.py
@@ -1,9 +1,16 @@
 import itertools
 import collections
 
+PART_ONE = False
+
+if PART_ONE:
+    letter_fn = lambda x: collections.Counter(x).most_common(1)[0][0]
+else:
+    letter_fn = lambda x: collections.Counter(x).most_common()[-1][0]
+
 print(
     ''.join(
-        collections.Counter(letters).most_common(1)[0][0]
+        letter_fn(letters)
         for letters in itertools.zip_longest(*open('day_6_input.txt'))
     )
 )
