78045369b7393c1a086a4b1f4544b159e3d1d1d2
Leo Hemsted <leohemsted@gmail.com>
2016-12-10 12:18:34 +0000

    day 7 part two
    not backwards compatible due to the difference between ABBA detection and ABA/BAB pairing
    but fairly self explanatory

--- day_7.py
+++ day_7.py
@@ -1,17 +1,21 @@
 import itertools
 
 
-def contains_abba(s):
+def get_abas(s):
     letter_counts = [(k, len(list(g))) for k, g in itertools.groupby(s)]
     # for all
-    return any(
-        letter_counts[i][1] == 2 and
-        letter_counts[i - 1][0] == letter_counts[i + 1][0]
-        for i in range(1, len(letter_counts) - 1)
-    )
+    for i in range(1, len(letter_counts) - 1):
+        if (
+            letter_counts[i][1] == 1 and
+            letter_counts[i - 1][0] == letter_counts[i + 1][0]
+        ):
+            yield ''.join([letter_counts[i - 1][0], letter_counts[i][0], letter_counts[i + 1][0]])
 
 
-def has_abba(ip_address):
+def get_bab(aba):
+    return ''.join([aba[1], aba[0], aba[1]])
+
+def chunk_ip_addr(ip_address):
     hypernets = []
     sequences = []
 
@@ -23,7 +27,19 @@ def has_abba(ip_address):
             hypernets.append(hypernet)
         sequences.append(chunk)
 
-    return not any(contains_abba(s) for s in hypernets) and any(contains_abba(s) for s in sequences)
+    return sequences, hypernets
 
 
-print(sum(has_abba(ip_address) for ip_address in open('day_7_input.txt')))
+def has_aba(ip_address):
+    sequences, hypernets = chunk_ip_addr(ip_address)
+
+    for sequence in sequences:
+        for aba in get_abas(sequence):
+            bab = get_bab(aba)
+            if any(bab in hypernet for hypernet in hypernets):
+                return True
+    return False
+
+
+
+print(sum(has_aba(ip_address) for ip_address in open('day_7_input.txt')))
