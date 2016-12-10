c34b7f937435c3bc1f52e12c88a6b111bf91f792
Leo Hemsted <leohemsted@gmail.com>
2016-12-10 12:38:50 +0000

    day 8 (complete)
    turns out my debugging print statements were part two for free, that's nice :)
    
    three separate regexes to match the commands - very straightforward. lets not overcomplicate things

--- day_8.py
+++ day_8.py
@@ -1,5 +1,9 @@
-WIDTH = 12
-HEIGHT = 4
+import re
+import itertools
+from functools import partial
+
+WIDTH = 50
+HEIGHT = 6
 
 class Screen(list):
     def __str__(self):
@@ -40,19 +44,44 @@ def print_screen(screen):
     x = ' '.join(str(x).ljust(2) for x in range(WIDTH)) + '\n'
     for y_ in range(HEIGHT):
         x += ('  '.join(
-            map(lambda z: '#' if screen[z][y_] else '.', range(WIDTH))
-        ))
+            '#' if screen[z][y_] else '.' for z in range(WIDTH))
+        )
         x += '\n'
     x += '\n'
     return x
 
 
+def get_command(line):
+    """
+    Return a callable pre-loaded with the arguments provided
+    """
+    RECT_STR = r'rect (?P<x>\d+)x(?P<y>\d+)'
+    ROW_STR = r'rotate row y=(?P<y>\d+) by (?P<dist>\d+)'
+    COL_STR = r'rotate column x=(?P<x>\d+) by (?P<dist>\d+)'
+    rect_match = re.match(RECT_STR, line)
+    row_match = re.match(ROW_STR, line)
+    col_match = re.match(COL_STR, line)
+    if rect_match:
+        return partial(rect, int(rect_match.group('x')), int(rect_match.group('y')))
+    if row_match:
+        return partial(rotate_row, int(row_match.group('y')), int(row_match.group('dist')))
+    if col_match:
+        return partial(rotate_column, int(col_match.group('x')), int(col_match.group('dist')))
 
-rect(x=3, y=2)
-print(screen)
-rotate_column(1, 1)
-print(screen)
-rotate_row(0, 4)
-print(screen)
-rotate_column(1, 1)
-print(screen)
+def count_lit_pixels():
+    return sum(itertools.chain.from_iterable(screen))
+
+data = open('day_8_input.txt')
+test_data = [
+    'rect 3x2',
+    'rotate column x=1 by 1',
+    'rotate row y=0 by 4',
+    'rotate column x=1 by 1',
+]
+for line in data:
+    x = get_command(line)
+    print(x)
+    x()
+    print(screen)
+
+print(count_lit_pixels())
